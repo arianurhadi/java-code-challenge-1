d374a0d7586e0956639a384e7a8fa578fc69ad82
arianurhadi <arianurhadi81@gmail.com>
2021-10-12 16:55:32 +0700

    first commit

---
+++ JavaBasicChallange.java
@@ -0,0 +1,22 @@
+class JavaBasicChallange {
+    public static void main(String[] args) {
+        String firstName = "Aria";
+        String lastName = "Nurhadi Zain";
+        int age = 20;
+        String phoneNumber = "08218391238";
+        String address = "Batam";
+        String status = "Mahasiswa";
+        String univ = "STMIK Akakom Yogyakarta";
+        String prodi = "Sistem Informasi";
+
+        System.out.println("Name : " + firstName + " " + lastName);
+        System.out.println("Last Name : " + lastName);
+        System.out.println("Age : " + age);
+        System.out.println("Phone Number : " + phoneNumber);
+        System.out.println("Address : " + address);
+        System.out.println("Status : " + status);
+        System.out.println("University : " + univ);
+        System.out.println("Study Program : " + prodi);
+
+    }
+}
